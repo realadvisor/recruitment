62a28e361612b4cb82c49a4b01693906d06f9f71
Jonas Wiesel <wieseljonas@users.noreply.github.com>
2018-11-19 02:16:17 +0100

    Revert "Working Heroku Deployment (#1)"
    This reverts commit d6f55446c5bdd861ea8d8a0c7986870349384cc4.

--- web/next.config.js
+++ web/next.config.js
@@ -3,14 +3,7 @@
 
 let API_ENDPOINT = process.env.API_ENDPOINT;
 
-const withBundleAnalyzer =
-  process.env.BUNDLE_ANALYZE != null
-    ? require('@zeit/next-bundle-analyzer')
-    : v => v;
-
-module.exports = withBundleAnalyzer({
-  pageExtensions: ['js'],
-
+export const nextConfig = {
   serverRuntimeConfig: Object.freeze({
     // Will only be available on the server side
     // mySecret: 'secret'
@@ -27,6 +20,17 @@ module.exports = withBundleAnalyzer({
       },
     ],
   },
+};
+
+const withBundleAnalyzer =
+  process.env.BUNDLE_ANALYZE != null
+    ? require('@zeit/next-bundle-analyzer')
+    : v => v;
+
+module.exports = withBundleAnalyzer({
+  pageExtensions: ['js'],
+
+  ...nextConfig,
   // bundle analizer
   analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
   analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
--- web/utils/getConfig.js
+++ web/utils/getConfig.js
@@ -1,8 +1,9 @@
 /* @flow */
 
 import nextGetConfig from 'next/config';
+import type { NextConfig } from '../next.config';
 
-export const getConfig = () => {
+export const getConfig: () => NextConfig = () => {
   // exports in nextjs are organized in manner exports = require('./dist/blabla')
   // in such case esm does not recognize that it could be possible es module
   // so we check on `default` export existance in runtime
