2d2dff3d84be0e5d3d14a3293b3c0ba472f73adc
Jonas Wiesel <wieseljonas@gmail.com>
2018-11-19 01:26:43 +0100

    add features

--- api/src/schema/PropertyType.js
+++ api/src/schema/PropertyType.js
@@ -1,6 +1,6 @@
 /* @flow */
 
-import { GraphQLObjectType, GraphQLFloat } from 'graphql';
+import { GraphQLObjectType, GraphQLFloat, GraphQLInt } from 'graphql';
 import { GraphQLDateTime } from 'graphql-iso-date';
 
 import { globalIdField } from 'graphql-relay';
@@ -12,9 +12,24 @@ export const PropertyType = new GraphQLObjectType({
   fields: () => ({
     id: globalIdField(),
 
+    numberOfRooms: {
+      type: GraphQLFloat,
+      resolve: parent => parent.number_of_rooms,
+    },
+
     livingSurface: {
       type: GraphQLFloat,
       resolve: parent => parent.living_surface,
+    },
+
+    landSurface: {
+      type: GraphQLFloat,
+      resolve: parent => parent.land_surface,
+    },
+
+    numberOfParkings: {
+      type: GraphQLInt,
+      resolve: parent => parent.number_of_parkings,
     },
 
     createdAt: {
